48e3d3261a73bafaf3d1327b9bed948788e51688
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2024-05-12 01:56:13 +0600

    yajra-datatable-crud-pivottable-select-to

--- resources/views/customers/create.blade.php
+++ resources/views/customers/create.blade.php
@@ -10,11 +10,9 @@
             <input type="text" class="form-control" id="customer_name" name="customer_name" >
         </div>
     
-    
         <div class="form-group mt-3">
             <label for="customer_email">Customer Email</label>
             <input type="email" class="form-control" id="customer_email" name="customer_email" aria-describedby="emailHelp" >
-          
         </div>
     
         <div class="form-group  mt-3">
@@ -25,12 +23,10 @@
             <label for="customer_bill">Customer Bill</label>
             <input type="number" class="form-control" id="customer_bill" name="customer_bill" >
         </div>
-    
-      
-     <div class="mt-2">
-    
-        <button type="submit" class="btn btn-primary btn-sm">Submit</button>
-    </div>
+
+        <div class="mt-2">
+            <button type="submit" class="btn btn-primary btn-sm">Submit</button>
+        </div>
     </form>
     
     
